c13fe32c68e8844817bf5619fa5730b89561ee24
Crowdbotics <team@crowdbotics.com>
2021-09-13 13:41:04 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "lane_hero_30501Identifier",
+  url: "https://lane_hero_30501Identifier.botics.co",
+  api: "https://lane_hero_30501Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
